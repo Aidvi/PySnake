69a0ef6307a71d755e0d6917d20dcda394e56016
Aidvi <nissen.magnus@outlook.dk>
2019-03-16 19:13:05 +0100

    sec commit

--- pysnake.py
+++ pysnake.py
@@ -25,12 +25,13 @@ class cube(object):
         cube_column = self.pos[1]
 
         pygame.draw.rect(surface, self.color, (cube_row*distance+1, cube_column*distance+1, distance-2, distance-2))
-        pygame.draw.rect(surface, self.color, (cube_row*distance+1, cube_column*distance+1, distance-2, distance-2))
 
 
 class snake(object):
     body = []
     turns = {}
+
+    i = 0
 
     def __init__(self, color, pos):
         self.color = color
@@ -39,7 +40,7 @@ class snake(object):
         self.dir_x = 0
         self.dir_y = 1
 
-    def move(self):
+    def event_handler(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -47,39 +48,45 @@ class snake(object):
             keys = pygame.key.get_pressed()
 
             for key in keys:
-                if keys[pygame.K_LEFT]:
+                if keys[pygame.K_LEFT] and (self.dir_y != 0 and self.dir_x != 1):
                     self.dir_x = -1
                     self.dir_y = 0
-                    self.turns[self.head.pos[:]] = [self.dir_x, self.dir_y]
-
-                elif keys[pygame.K_RIGHT]:
-
+                elif keys[pygame.K_RIGHT] and (self.dir_y != 0 and self.dir_x != -1):
                     self.dir_x = 1
                     self.dir_y = 0
-                    self.turns[self.head.pos[:]] = [self.dir_x, self.dir_y]
-
-                elif keys[pygame.K_UP]:
-
+                elif keys[pygame.K_UP] and (self.dir_y != 1 and self.dir_x != 0):
                     self.dir_x = 0
                     self.dir_y = -1
-                    self.turns[self.head.pos[:]] = [self.dir_x, self.dir_y]
-
-                elif keys[pygame.K_DOWN]:
-
+                elif keys[pygame.K_DOWN] and (self.dir_y != -1 and self.dir_x != 0):
                     self.dir_x = 0
                     self.dir_y = 1
-                    self.turns[self.head.pos[:]] = [self.dir_x, self.dir_y]
+                self.turns[self.head.pos[:]] = [self.dir_x, self.dir_y]
+        self.move()
+
+    def move(self):
+        self.i += 1
+
+        if(self.i >= 60):
+            self.i = 0
+
+        speed = len(self.body)
+        if(speed <= 2):
+            speed = 2
+
+        if(self.i % speed == 0): ## Modulus operator
+            return
 
         for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
                 turn = self.turns[p]
                 c.move(turn[0], turn[1])
-                if i == len(self.body) -1: ## If we are on the last cube/turn, it needs to stop that cube from turning too, so we remove from list
+                if i == len(
+                        self.body) - 1:  ## If we are on the last cube/turn, it needs to stop that cube from turning too, so we remove from list
                     self.turns.pop(p)
 
-            else:   ## Checks if we are on the edge of the screen
-                    ## Runs through scenarios where if we hit an edge of the screen, it will place the snake accordingly on the other edge. If non of that is true we simply move
+            else:  ## Checks if we are on the edge of the screen
+                ## Runs through scenarios where if we hit an edge of the screen, it will place the snake accordingly on the other edge. If non of that is true we simply move
                 if c.dir_x == -1 and c.pos[0] <= 0:
                     c.pos = (c.rows - 1, c.pos[1])
                 elif c.dir_x == 1 and c.pos[0] >= c.rows - 1:
@@ -90,6 +97,7 @@ class snake(object):
                     c.pos = (c.pos[0], c.rows - 1)
                 else:
                     c.move(c.dir_x, c.dir_y)
+
 
     def reset(self, pos):
         self.head = cube(pos)
@@ -122,69 +130,56 @@ class snake(object):
             else:
                 c.draw(surface)
 
+class game():
+    def __init__(self):
+        self.width = 500
+        self.height = 500
+        self.rows = 20
+        self.player = snake((255, 0, 0), (10, 10))
+        self.snack = cube(self.random_snack(self.rows, self.player), color=(0, 255, 0))
 
-def draw_grid(width, rows, surface):
-    grid_size = width // rows
+    def redraw_window(self,window):
+        window.fill((0, 0, 0))
+        self.player.draw(window)
+        self.snack.draw(window)
+        pygame.display.update()
 
-    x = 0
-    y = 0
-    for l in range(rows):
-        x = x + grid_size
-        y = y + grid_size
+    def random_snack(self,rows, player):
+        positions = player.body
 
-        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, width))
-        pygame.draw.line(surface, (255, 255, 255), (0, y), (width, y))
-
-
-def redraw_window(surface):
-    global rows, width, player, snake
-    surface.fill((0, 0, 0))
-    player.draw(surface)
-    snack.draw(surface)
-    draw_grid(width, rows, surface)
-    pygame.display.update()
-
-
-def random_snack(rows, player):
-    positions = player.body
-
-    while True:
-        x = random.randrange(rows)
-        y = random.randrange(rows)
-        if len(list(filter(lambda it: it.pos == (x, y), positions))) > 0:
-            continue
-        else:
-            break
-
-    return x, y
-
-
-def main():
-    global width, rows, player, snack
-    width = 500
-    height = 500
-    rows = 20
-    window = pygame.display.set_mode((width, height))
-    player = snake((255, 0, 0), (10, 10))
-    snack = cube(random_snack(rows, player), color=(0, 255, 0))
-    game = True
-
-    clock = pygame.time.Clock()  ## Does so the game does not run more than 10 frames per sec
-    while game:
-        pygame.time.delay(50)  ## Delays the game with a few milisec so it doesnt run too fast
-        clock.tick(10)
-        player.move()
-        if player.body[0].pos == snack.pos:
-            player.add_cube()
-            snack = cube(random_snack(rows, player), color=(0, 255, 0))
-
-        for x in range(len(player.body)):
-            if player.body[x].pos in list(map(lambda it: it.pos, player.body[x + 1:])):
-                print("Score: ", len(player.body))
-                player.reset((10, 10))
+        while True:
+            x = random.randrange(rows)
+            y = random.randrange(rows)
+            if len(list(filter(lambda it: it.pos == (x, y), positions))) > 0:
+                continue
+            else:
                 break
 
-        redraw_window(window)
+        return x, y
 
-    pass
-main()
+
+    def start(self):
+        window = pygame.display.set_mode((self.width, self.height))
+        game = True
+
+        clock = pygame.time.Clock()  ## Does so the game does not run more than 10 frames per sec
+        while game:
+            pygame.time.delay(50)  ## Delays the game with a few milisec so it doesnt run too fast
+            clock.tick(60)
+            self.player.event_handler()
+            if self.player.body[0].pos == self.snack.pos:
+                self.player.add_cube()
+                self.snack = cube(self.random_snack(self.rows, self.player), color=(0, 255, 0))
+
+            for x in range(len(self.player.body)):
+                if self.player.body[x].pos in list(map(lambda it: it.pos, self.player.body[x + 1:])):
+                    print("Score: ", len(self.player.body))
+                    self.player.reset((10, 10))
+                    break
+
+            self.redraw_window(window)
+
+        pass
+game = game()
+game.start();
+
